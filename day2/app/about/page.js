447eb3abb23547fc0cc082a1e62869dd3d5a4fd2
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-08-26 20:04:23 +0530

    day2

---
+++ day2/app/about/page.js
@@ -0,0 +1,11 @@
+const about = () => {
+  return (
+    <>
+      <div>
+        i am about page
+      </div>
+    </>
+  )
+}
+
+export default about
---
+++ day2/app/contact/page.js
@@ -0,0 +1,7 @@
+const contact = () => {
+  return (
+    <div>I am contact page</div>
+  )
+}
+
+export default contact
