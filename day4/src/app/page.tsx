2a697fda0f26f2b1d6f7bf2f23114bca47f7d769
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-08-31 23:49:13 +0530

    whychooseus added

--- day4/src/app/page.tsx
+++ day4/src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCard from "@/components/FeaturedCard";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
         <main className="min-h-screen text-white bg-black/[0.96] antialiased bg-grid-white/[0.02]">
             <HeroSection/>
             <FeaturedCard/>
+            <WhyChooseUs/>
         </main>
     </>
   );
