6458266210869a6faf83fd50e7ec3542fd7496d4
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-07 22:44:44 +0530

    courses page added

--- MusicWebsite/src/app/page.tsx
+++ MusicWebsite/src/app/page.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
-        <main className="min-h-screen text-white bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+        <main className="min-h-screen text-white bg-black/[0.96] antialiased  ">
             <HeroSection/>
             <FeaturedCard/>
             <WhyChooseUs/>
