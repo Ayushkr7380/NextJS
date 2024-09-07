6a2eb9a4d3ea83e9c5da69f221db3367d94fedf2
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-09-07 17:11:53 +0530

    spotlight added

--- MusicWebsite/tailwind.config.ts
+++ MusicWebsite/tailwind.config.ts
@@ -3,6 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const svgToDataUri = require("mini-svg-data-uri"); 
 
 const colors = require("tailwindcss/colors");
+
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
@@ -60,11 +61,22 @@ const config: Config = {
       animation: {
         scroll:
           "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+          spotlight: "spotlight 2s ease .75s 1 forwards",
       },
       keyframes: {
         scroll: {
           to: {
             transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+        spotlight: {
+          "0%": {
+            opacity: '0',
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: '1',
+            transform: "translate(-50%,-40%) scale(1)",
           },
         },
       },
